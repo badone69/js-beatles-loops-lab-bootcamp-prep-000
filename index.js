7e140fa088d9bdab411f6067d3d13a58a3134c74
student online <eliel.badon@gmail.com>
2018-07-29 22:58:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function iLoveTheBeatles(n){
   var array = [];
   do {
     array.push("I love the Beatles!" );
-    num++;
-  } while (num < 15);
+    n++;
+  } while (n < 15);
   return array;
 }
