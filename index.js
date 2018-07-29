80200d5522d30fda03472c12b8cb7d053debe90c
student online <eliel.badon@gmail.com>
2018-07-29 22:16:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,12 +7,7 @@ function theBeatlesPlay(musician, instrument){
   return array;
 }
 
-function johnLennonFacts(){
-  var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"]; 
+function johnLennonFacts(facts){
   var exclfacts = [];
   var i = 0;
   while (i < facts.length) {
